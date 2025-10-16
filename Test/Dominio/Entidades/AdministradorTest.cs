a40628f5900b0c15670fe8c4c86c36da18016c07
Ryan Marcello <ryan.rmasa@gmail.com>
2025-10-16 15:52:23 -0300

    Criando testes de unidade para os modelos de administrador e veiculo

---
+++ Test/Dominio/Entidades/AdministradorTest.cs
@@ -0,0 +1,25 @@
+﻿using GerenciadorDeVeiculos.Dominio.Entidades;
+namespace Test.Dominio.Entidades;
+
+[TestClass]
+public class AdministradorTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        //arrange
+        var adm = new Administrador();
+
+        //Act
+        adm.Id = 1;
+        adm.Email = "teste@teste.com";
+        adm.Senha = "Teste";
+        adm.Perfil = "Adm";
+
+        //Assert
+        Assert.AreEqual(1, adm.Id);
+        Assert.AreEqual("teste@teste.com", adm.Email);
+        Assert.AreEqual("Teste", adm.Senha);
+        Assert.AreEqual("Adm", adm.Perfil);
+    }
+}
---
+++ Test/Dominio/Entidades/VeiculoTest.cs
@@ -0,0 +1,25 @@
+﻿using GerenciadorDeVeiculos.Dominio.Entidades;
+namespace Test.Dominio.Entidades;
+
+[TestClass]
+public class VeiculoTest
+{
+    [TestMethod]
+    public void TestarGetSetPropriedades()
+    {
+        //arrange
+        var veiculo = new Veiculo();
+
+        //Act
+        veiculo.Id = 1;
+        veiculo.Nome = "veiculoNome";
+        veiculo.Marca = "veiculoMarca";
+        veiculo.Ano = 1886;
+
+        //Assert
+        Assert.AreEqual(veiculo.Id, 1);
+        Assert.AreEqual(veiculo.Nome, "veiculoNome");
+        Assert.AreEqual(veiculo.Marca, "veiculoMarca");
+        Assert.AreEqual(veiculo.Ano, 1886);
+    }
+}
